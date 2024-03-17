049b744763b7a083a6e5be21d9ac9bbaf1027451
fres-sudo <francesco.calicchio@studenti.unicam.com>
2024-03-17 20:50:00 +0100

    updates

--- api/index.js
+++ api/index.js
@@ -81,4 +81,44 @@ app.all('*', (req, res, next) => {
 
 app.use(globalErrorHandler);
 
-export default app;
+import { connect } from 'mongoose';
+import { config } from 'dotenv';
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+  
+config({ path: './config.env' });
+
+const DB = process.env.DATABASE;
+
+// Log that the script is starting...
+console.log('Starting server script...');
+
+// Connect to the database
+console.log('Connecting to the database...');
+connect(DB)
+  .then(() => {
+    console.log('DB connection successful!✅');
+    // Start the app after successful database connection
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+      console.log(`App running on port ${port}...`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to the database:', err);
+    process.exit(1);
+  });
+
+// Log that the script has finished
+console.log('Server script finished execution.👀');
+
+// Handle unhandled rejections
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
