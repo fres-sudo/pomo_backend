313b9b3955b465b8147d59b06954b2d12c53c119
fres-sudo <francesco.calicchio@studenti.unicam.com>
2024-03-17 18:28:23 +0100

    updates

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use('/api/v1/tasks', taskRouter);
 app.use('/api/v1/projects', projectRouter);
 app.use('/api/v1/users', userRouter);
 
-app.get("/", getAllUsers ) 
+app.get("/", getAllUsers) 
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
