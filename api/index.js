f180117298d4ffe9dbbfc1f5c457b38a95a18f34
fres-sudo <francesco.calicchio@studenti.unicam.com>
2024-03-17 21:07:14 +0100

    updates

--- api/index.js
+++ api/index.js
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// 3) ROUTES
+// 3)  ROUTES
 app.use('/api/v1/tasks', taskRouter);
 app.use('/api/v1/projects', projectRouter);
 app.use('/api/v1/users', userRouter);
